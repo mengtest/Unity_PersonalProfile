ec1888f19b685e4bd6c868514f572a50583572a0
Super0410 <970173129@qq.com>
2016-09-18 20:29:33 +0800

    Final Homework 2

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("5");
+var x = process.argv[2];
+var num = parseInt(x);
+var a, b, c;
+var star, space;
+for (a = 0; a < num; a++) {
+    star = "";
+    space = "";
+    for (b = 0; b < (num - a - 1); b++) {
+        space += " ";
+    }
+    for (c = 0; c < (2 * a + 1); c++) {
+        star += "*";
+    }
+    console.log(space + star);
+}
--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-console.log("5");
+var x = process.argv[2];
+var num: number = parseInt(x);
+
+var a, b, c: number;
+var star, space: string;
+
+for (a = 0; a < num; a++) {
+    star = "";
+    space = "";
+    for (b = 0; b < (num - a - 1); b++) {
+        space += " ";
+    }
+    for (c = 0; c < (2 * a + 1); c++) {
+        star += "*";
+    }
+    console.log(space + star);
+}
